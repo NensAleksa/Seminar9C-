9ccc3b0c4b50f64c7738be6fff4fbef4445edf59
Anastasia <nensy.gri3108@mail.ru>
2022-07-25 19:05:49 +0300

    Создали программу1

---
+++ num1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+Console.Clear();
+Console.WriteLine($"Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.");
+int n = InputNumbers("Введите M: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
+
+void PrintNumber(int n, int count)
+{
+  if (count > n) return;
+  PrintNumber(n, count + 1);
+  Console.Write(count + ", ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+        }
+    }
+}
